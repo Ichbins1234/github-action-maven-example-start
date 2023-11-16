d09bfc60597f75728f2a8377e829a66dbe53542a
Ichbins1234 <fgaertne@htl-steyr.ac.at>
2023-11-16 13:52:21 +0100

    JUnit

---
+++ src/test/java/DeepThoughtTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeepThoughtTest {
+
+    @Test
+    void answer_to_the_ultimate_question_of_life_the_universe_and_everything() {
+        DeepThought superComputer = new DeepThought();
+        assertEquals(42,superComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything());
+    }
+}
